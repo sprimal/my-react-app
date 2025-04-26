8fd07cabd7348c178c5a90e8e298a39bc4fe309c
sprimal <swoumyarimal@gmail.com>
2025-04-25 22:20:28 -0400

    initial commit for props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,8 @@
 // import Footer from './FOOTER.JSX';
 // import Cars from './Cars.jsx';
 // import Card from './Card.jsx';
-import Button from './Button.jsx';
+// import Button from './Button.jsx';
+import Student from "./Student.jsx";
 
 function App() {
   return (
@@ -17,9 +18,15 @@ function App() {
     // <Card />
     // <Card />
     // </>
-
-    <Button />
-
+    // <Button />
+    <>
+        <Student name= "swoumya" age={19} isStudent={true} />
+        <Student name="nirmal" age={21} isStudent={true} />
+        <Student name="aayush" age={20} isStudent={false} />
+        <Student name="rohit" age={22} isStudent={true} />
+        <Student name="kabin" age={22} isStudent={true} />
+        <Student/>
+    </> 
   );
 }
 
